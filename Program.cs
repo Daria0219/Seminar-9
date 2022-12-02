f553076e90efcc035f93673e5b81eb7f800d2954
Your Name <you@example.com>
2022-12-02 20:39:05 +0400

    homework №1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// 1. Программа, которая выведет все натуральные числа в промежутке от N до 1.
+
+Console.WriteLine("Введите число N (N больше 0)");
+int N =int.Parse(Console.ReadLine());
+string Function(int n){
+    if (n==1) return 1.ToString();
+    else { 
+    return (n+" "+Function(n-1));}}
+
+Console.WriteLine(Function(N));
