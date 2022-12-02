37117a775052582583800000b2465529edb17920
Your Name <you@example.com>
2022-12-02 20:46:41 +0400

    homework №2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// 1. Программа, которая выведет все натуральные числа в промежутке от N до 1.
+﻿/* 1. Программа, которая выведет все натуральные числа в промежутке от N до 1.
 
 Console.WriteLine("Введите число N (N больше 0)");
 int N =int.Parse(Console.ReadLine());
@@ -8,3 +8,17 @@ string Function(int n){
     return (n+" "+Function(n-1));}}
 
 Console.WriteLine(Function(N));
+*/
+
+// Программа, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число N и M (N больше M)");
+int N =int.Parse(Console.ReadLine());
+int M =int.Parse(Console.ReadLine());
+
+ int Function(int n, int m){
+    if (n==m) return m;
+    else { 
+    return (m+Function(n, m+1));}}
+
+Console.WriteLine(Function(N, M));
